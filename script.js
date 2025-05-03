f80715acf69bc42b67a6cd56b6df261e6a131110
Lucas Jackson <futurelucas123@gmail.com>
2025-05-03 20:37:44 +0000

    .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function switchScene() {
     const scene1 = document.getElementById("scene1")
     const scene2 = document.getElementById("scene2")
@@ -6,10 +7,12 @@ function switchScene() {
         scene1.style.display = 'none';
         scene2.style.display = 'block';
     }
-    /*
+    
     else {
         scene1.style.display = 'block';
         scene2.style.display = 'none';
     }
-    */
+
 }
+*/
+
