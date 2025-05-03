a27709746ea1bc14e18a5d42086d663f7e95cb13
Lucas Jackson <futurelucas123@gmail.com>
2025-05-03 22:05:22 +0000

    button test

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function switchScene() {
 }
 */
 var activePage = 0
-
+/*
 function homePage() {
     const home = document.getElementById("home")
     const project1 = document.getElementById("project1")
@@ -56,5 +56,20 @@ function project2Page() {
         project2.style.display = 'block';
         
         activePage = 2
+    }
+}
+    */
+
+function pageSwitch(pageNumber, pageBlock, pageNone1, pageNone2) {
+    const home = document.getElementById("home")
+    const project1 = document.getElementById("project1")
+    const project2 = document.getElementById("project2")
+
+    if (activePage !== pageNumber) {
+        pageBlock.style.display = 'block';
+        pageNone1.style.display = 'none';
+        pageNone2.style.display = 'none';
+        
+        activePage = pageNumber
     }
 }
