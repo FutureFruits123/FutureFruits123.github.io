c7527dc4c65b89067b26b9d5bfe99bb40727674b
Lucas Jackson <futurelucas123@gmail.com>
2025-05-04 17:39:00 +0000

    page switch test

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var activePage = 0
+//var activePage = 0
 
 function pageSwitch(pageNumber, pageBlock, pageNone1, pageNone2) {
     const home = document.getElementById("home")
     const project1 = document.getElementById("project1")
     const project2 = document.getElementById("project2")
 
-    if (activePage !== pageNumber) {
+    /*if (activePage !== pageNumber) {
         pageBlock.style.display = 'block';
         pageNone1.style.display = 'none';
         pageNone2.style.display = 'none';
         
         activePage = pageNumber
-    }
+    }*/
+
+    pageBlock.style.display = 'block';
+    pageNone1.style.display = 'none';
+    pageNone2.style.display = 'none';
+        
+
+    
 }
