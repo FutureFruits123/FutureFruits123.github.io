971f9902e884211b22c53cbfecdcd52ebd3b0056
Lucas Jackson <futurelucas123@gmail.com>
2025-05-04 19:44:40 +0000

    .

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ function pageSwitch(pageNumber, pageBlock, pageNone1, pageNone2) {
         pageNone1.style.display = 'none';
         pageNone2.style.display = 'none';
         
-        print(activePage)
         activePage = pageNumber
-        print(activePage)
-        print(pageBlock)
     }
 }
